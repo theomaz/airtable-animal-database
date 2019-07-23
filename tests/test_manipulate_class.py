37e227f278ba2f110fb7df449b0d012f5c34c9b6
theofilos-mazidzoglou <53102790+theofilos-mazidzoglou@users.noreply.github.com>
2019-07-23 10:46:12 -0400

    Changed test to pytest

--- tests/test_manipulate_class.py
+++ tests/test_manipulate_class.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ADP_dir.ADP import *
 from session_dir.session_object import *
 
@@ -5,13 +7,9 @@ def test_get_date_born():
     """
     Verify get_date_born returns date_weaned minus 21 days
     """
-    assert get_date_born("7/21/2019") == "6/30/2019",\
-           "get_date_born(\"7/21/2019\") does not return \"6/30/2019\""
-    assert get_date_born("3/7/2019") == "2/14/2019",\
-           "get_date_born(\"3/7/2019\") does not return \"2/14/2019\""
-    assert get_date_born("1/2/2019") == "12/12/2018",\
-           "get_date_born(\"1/2/2019\") does not return \"12/12/2018\""
-
-if __name__ == "__main__":
-    test_get_date_born()
-    print("Everything passed")
+    assert get_date_born("7/21/2019") == "6/30/2019"
+           #"get_date_born(\"7/21/2019\") does not return \"6/30/2019\""
+    assert get_date_born("3/7/2019") == "2/14/2019"
+           #"get_date_born(\"3/7/2019\") does not return \"2/14/2019\""
+    assert get_date_born("1/2/2019") == "12/12/2018"
+           #"get_date_born(\"1/2/2019\") does not return \"12/12/2018\""
